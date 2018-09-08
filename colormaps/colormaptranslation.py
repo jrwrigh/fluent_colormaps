043501dc5de2e4608e171c53fe6f77e9eb536757
u2berggeist <jrwrigh.iii@gmail.com>
2018-09-08 12:30:19 -0400

    made python translation script

---
+++ colormaps/colormaptranslation.py
@@ -0,0 +1,30 @@
+import csv
+from pathlib import Path
+import os
+scriptpath = Path(os.path.dirname(os.path.abspath(__file__)))
+
+## Inputs
+csv_path = scriptpath / './original_csvs/extended-black-body-table-float-0128.csv'
+colormapfilename = scriptpath / 'extended_blackbody.colormap'
+colormapname = 'ext-blkbdy'
+csv_has_header = True
+
+# Other stuff:
+
+header = f'("{colormapname}"\n'
+
+with csv_path.open() as csvfile:
+    csvreader = csv.reader(csvfile)
+    # if csv_has_header: fiel
+    with open(colormapfilename, 'w+') as file:
+        file.write(header)
+        for line in csvreader:
+            try:
+                # if first entry fails to convert to float, skip the line
+                float(line[0])
+            except:
+                pass
+            else:
+                n = [float(x) for x in line]
+                line_to_write = f'({n[0]} {n[1]} {n[2]} {n[3]})'
+                file.write(line_to_write + '\n')
