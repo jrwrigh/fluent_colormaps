97eb944c720ff4bb4b4b8ef9da0f8b62d5df160e
u2berggeist <jrwrigh.iii@gmail.com>
2018-09-08 12:56:34 -0400

    added pdf, updated README, commented translation script

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # fluent_colormaps
-Repo with tools and to make setting color maps easier
+Here are tools that allow users to use different color maps in ANSYS Fluent without having to enter then in manually or have them buried in a settings file. It includes a scheme file that allows simple import/export of colormaps as well as some selected colormaps.
+
+The scheme script for importing the color maps to Fluent is taken from [this](http://www.cadfamily.com/download-pdf/FLUENT12/AutoUGM03_fluent_tips.pdf) pdf presentation. It is also included in this repository for archiving purposes.
+
+The example colormaps (other than `thermacam.colormap` which came from the presentation linked above) are taken from [Kenneth Moreland's webpage](https://www.kennethmoreland.com/color-advice/) on color map advice.
+
+I've also written up a simple Python script that will translate CSV's into the correct format for this process. It is commented with instructions.
+
+## Use:
+
+In Fluent TUI:
+```
+> (load "rw_colormap.scm")
+Loading ".\.\rw_colormap.scm"
+Done.
+#f
+```
+This will add `read-colormap` and `write-colormap` functions to the `/file` menu in the TUI:
+
+```
+> file/read-colormap "kindlmann_extended.colormap"
+```
+
+After you do that, the color map will be available for use.
+
+
--- colormaps/colormaptranslation.py
+++ colormaps/colormaptranslation.py
@@ -1,17 +1,22 @@
 import csv
 from pathlib import Path
 import os
+    # get path of the script
 scriptpath = Path(os.path.dirname(os.path.abspath(__file__)))
 
-## Inputs
+## Inputs:
+    # Path to the CSV file
 csv_path = scriptpath / './original_csvs/extended-kindlmann-table-float-0128.csv'
+    # Name and location for the colormap to be created
 colormapfilename = scriptpath / 'kindlmann_extended.colormap'
-colormapname = 'kindlmann-extended'
+    # Name that Fluent uses to reference the colormap
+fluentcolormapname = 'kindlmann-extended'
+    # Does CSV have header?
 csv_has_header = True
 
-# Other stuff:
+## Translation:
 
-header = f'("{colormapname}"\n'
+header = f'("{fluentcolormapname}"\n'
 
 with csv_path.open() as csvfile:
     csvreader = csv.reader(csvfile)
@@ -19,11 +24,16 @@ with csv_path.open() as csvfile:
     with open(colormapfilename, 'w+') as file:
         file.write(header)
         for line in csvreader:
-            try:
-                # if first entry fails to convert to float, skip the line
-                float(line[0])
-            except:
-                pass
+            if csv_has_header:
+                try:
+                    # if first entry fails to convert to float, skip the line
+                    float(line[0])
+                except:
+                    pass
+                else:
+                    n = [float(x) for x in line]
+                    line_to_write = f'({n[0]} {n[1]} {n[2]} {n[3]})'
+                    file.write(line_to_write + '\n')
             else:
                 n = [float(x) for x in line]
                 line_to_write = f'({n[0]} {n[1]} {n[2]} {n[3]})'
